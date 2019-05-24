910b9324df206784a384ce20959130e2b8c7dc71
Christopher Bailey <cbailey@mort.is>
2019-05-24 11:33:54 -0400

    bumps version

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     test_suite="tests",
     tests_require=requirements["dev"],
     url="https://github.com/AngellusMortis/sxm-player",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
     extras_require={"dev": requirements["dev"]},
 )
